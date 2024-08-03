83e4ca077d9cbe24d7be8a91dadaff80e69d9b91
Ofer Klein <ofer@podium-media.com>
2024-08-03 19:53:58 +0300

    Deleting console logs

--- src/config/sequelize.ts
+++ src/config/sequelize.ts
@@ -6,12 +6,6 @@ const db_user = process.env.DB_USER!;
 const db_password = process.env.DB_PASSWORD!;
 const db_host = process.env.DB_HOST!;
 
-console.log(db_name,'DB NAME');
-console.log(db_user,'DB USER');
-console.log(db_password,'DB PASSWORD');
-console.log(db_host,'DB HOST');
-
-
 const sequelizeConnection = new Sequelize(db_name, db_user, db_password, {
     host:db_host,
     dialect: 'mysql',
